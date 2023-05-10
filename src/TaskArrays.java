10198fdff0f525d34968b55662df8c1c4f3262e0
Maksim Sumar <klfusdzen@gmail.com>
2023-05-10 10:26:20 +0300

    + 1 task

--- src/TaskArrays.java
+++ src/TaskArrays.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 //…
 //20
 public class TaskArrays {
-    public static void main(String[] args) {
+    /* public static void main(String[] args) {
         int a = 0;
         for (int i = 2; i <= 20; i++) {
             if (i % 2 == 0) a++;
@@ -24,6 +24,26 @@ public class TaskArrays {
         System.out.println(" ");
         for (int i = 0; i < Array.length; i++) {
             System.out.println(Array[i]);
+        }
+    } */
+
+    //Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3 1).
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 != 0) a++;
+        }
+        int[] Array = new int[a];
+        for (int i = 1, b = 0; i <= 99; i++) {
+            if (i % 2 != 0) {
+                Array[b] = i;
+                System.out.print(Array[b] + " ");
+                b++;
+            }
+        }
+        System.out.println(" ");
+        for (int i = Array.length - 1; i >= 0; i--) {
+            System.out.print(Array[i] + " ");
         }
     }
 }
